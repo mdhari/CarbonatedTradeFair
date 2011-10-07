07fd0f8f20a49e53948a0e960b40badc6890ea0b
Michael Hari <michaeldhari@gmail.com>
2011-10-06 21:28:04 -0700

    adding QuickProcessBean class

---
+++ src/edu/sjsu/carbonated/server/QuickProcessBean.java
@@ -0,0 +1,18 @@
+package edu.sjsu.carbonated.server;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class QuickProcessBean
+ */
+@Stateless
+public class QuickProcessBean implements QuickProcessBeanRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public QuickProcessBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ src/edu/sjsu/carbonated/server/QuickProcessBeanRemote.java
@@ -0,0 +1,7 @@
+package edu.sjsu.carbonated.server;
+import javax.ejb.Remote;
+
+@Remote
+public interface QuickProcessBeanRemote {
+
+}
