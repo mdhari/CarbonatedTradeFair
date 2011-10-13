e34e2891d2103ed741a5f4e950784e0f198e2cf8
Michael Hari <michaeldhari@gmail.com>
2011-10-12 23:09:29 -0700

    adding graphing from google chart api

--- src/edu/sjsu/carbonated/server/QuickProcessBean.java
+++ src/edu/sjsu/carbonated/server/QuickProcessBean.java
@@ -1,9 +1,26 @@
 package edu.sjsu.carbonated.server;
 
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ejb.Stateless;
+
+import org.apache.http.HttpConnection;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 /**
  * @author Michael Hari
@@ -24,7 +41,5 @@ public class QuickProcessBean implements QuickProcessBeanRemote {
 		return csvToJson.convertCSVFile(filename);
 
 	}
-    
-    
-
+   
 }
